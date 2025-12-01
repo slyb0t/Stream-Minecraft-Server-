40ad5e91cc666364d71e179be66e85dfdce0196f
franklin <frank378@gmail.com>
2025-11-30 17:31:28 -0700

    update the bash scripts

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,84 +1,45 @@
 #!/usr/bin/env bash
 #
-# SPDX-FileCopyrightText: ©2021-2025 franklin <smoooth.y62wj@passmail.net>
+# SPDX-FileCopyrightText: © 2025 franklin <smoooth.y62wj@passmail.net>
 #
 # SPDX-License-Identifier: MIT
 
 # ChangeLog:
 #
-# v 0.1 11/10/25 franklin - initial version
+# v 0.1 11/30/25 franklin - initial version
 
-BACKUP_DIR="/opt/backup"
+BACKUP_DIR="/tmp"
 DEB_PKG=(git gnupg)
 LRED='\033[1;31m'
-MC_HOME="/opt/mcserver"
+MC_HOME="/home/betty"
 MC_LOG="/var/log/minecraft"
+ME_SU=false
 
-function setup_logging() {
-  sudo mkdir -p "${MC_LOG}"
+if [ -f "${MC_HOME}/bin/common.sh" ]; then
+  source "${MC_HOME}/bin/common.sh"
+else
+  echo -e "${LRED}can not find common.sh.${NC}"
+  exit 1
+fi
+log_info "successfully sourced common.sh" && echo -e "\n"
 
-  mkdir -p "${MC_HOME}/workspace" # betty owns this dir
+if sudo -v &> /dev/null ;then
+  log_info "sudo is allowed"
+  ME_SU=true
+else
+  log_warn "sudo is not allowed"
+fi
 
-  sudo chown -R betty:games "${MC_LOG}" "${MC_HOME}/workspace" # change the perms
+var=$(date +"%FORMAT_STRING")
+now=$(date +"%m_%d_%Y")
+printf "%s\n" $now
+today=$(date +"%Y-%m-%d")
+printf "Backup from to ${MC_HOME} ${BACKUP_DIR}/backups/$(date +%A)/betty-mc-${today}.tar.xz"
 
-  if [ ! -e "${MC_HOME}/logs" ]; then
-    log_info "creating link to logs folder"
-    ln -s "${MC_LOG}" "${MC_HOME}/logs"
-  fi
-}
+if [ ! -d "${BACKUP_DIR}/backups/$(date +%A)/" ] ; then 
 
-function setup_java() {
-  log_header "Setup java JDK"
-  sudo apt install -y default-jdk
-}
+  mkdir "${MC_HOME}/backups"
+fi
 
-function setup_backup() {
-  git config --global --add safe.directory "${MC_HOME}"
-  localectl set-locale LANG=en_US.UTF-8
-  if [ ! -d "${BACKUP_DIR}" ]; then
-    log_info "Creating backup dir: ${BACKUP_DIR}"
-    sudo mkdir "${BACKUP_DIR}"
-  fi
-}
 
-function manage_region_files() {
-  if [ ! -d "${MC_HOME}/workspace/Minecraft-Region-Fixer" ]; then
-    log_info "Install Region Fixer"
-    git clone https://github.com/Fenixin/Minecraft-Region-Fixer.git "${MC_HOME}/workspace/Minecraft-Region-Fixer"
-  fi
-
-  log_info "Validate the region files"
-  python3 "${MC_HOME}/workspace/Minecraft-Region-Fixer/regionfixer.py" "${MC_HOME}/world"
-
-  # resotre the bad ones from backups
-  # python3 regionfixer.py -p 4 --replace-wrong --backups
-
-}
-
-function fix_permissions() {
-  chmod 664 "${MC_HOME}/world/poi/*"
-}
-
-function perform_function() {
-  sudo cp -Rp "${MC_HOME}" "${BACKUP_DIR}"
-}
-
-function main() {
-  if [ -f "${MC_HOME}/bin/common.sh" ]; then
-    source "${MC_HOME}/bin/common.sh"
-  else
-    echo -e "${LRED}can not find common.sh.${NC}"
-    exit 1
-  fi
-  log_info "successfully sourced common.sh" && echo -e "\n"
-
-  setup_logging
-  # setup_java
-  setup_backup
-  install_debian
-  manage_region_files
-  perform_function
-
-}
-
-main "$@"
+cp -r "${MC_HOME}/world
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -Xms3500M -Xmx3500M -jar minecraft_server_1.21.10.jar nogui
+
